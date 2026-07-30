b6ce77aca602a177f7ca6f9871dbff93a7c5e4cb
desireedias <desireerdias@gmail.com>
2026-07-30 09:31:28 -0400

    feat(auth): add authetication to API and update tests

--- product/tests/test_viewsets/test_product_viewset.py
+++ product/tests/test_viewsets/test_product_viewset.py
@@ -3,6 +3,7 @@ import json
 from django.urls import reverse
 from rest_framework.test import APIClient, APITestCase
 from rest_framework import status
+from rest_framework.authtoken.models import Token
 
 from order.factories import UserFactory
 from product.factories import CategoryFactory, ProductFactory
@@ -15,6 +16,8 @@ class TestProductViewSet(APITestCase):
 
     def setUp(self):
         self.user = UserFactory()
+        token = Token.objects.create(user=self.user)
+        token.save()
 
         self.product = ProductFactory(
             title="pro controller",
@@ -22,16 +25,25 @@ class TestProductViewSet(APITestCase):
         )
 
     def test_get_all_products(self):
+        token = Token.objects.get(user__username=self.user.username)
+        self.client.credentials(
+            HTTP_AUTHORIZATION=f"Token {token.key}"
+        )
         response = self.client.get(reverse("product-list", kwargs={"version": "v1"}))
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
         product_data = json.loads(response.content)
 
-        self.assertEqual(product_data[0]["title"], self.product.title)
-        self.assertEqual(product_data[0]["price"], self.product.price)
-        self.assertEqual(product_data[0]["active"], self.product.active)
+        self.assertEqual(product_data["results"][0]["title"], self.product.title)
+        self.assertEqual(product_data["results"][0]["price"], self.product.price)
+        self.assertEqual(product_data["results"][0]["active"], self.product.active)
 
     def test_create_product(self):
+        token = Token.objects.get(user__username=self.user.username)
+        self.client.credentials(
+            HTTP_AUTHORIZATION=f"Token {token.key}"
+        )
         category = CategoryFactory()
         data = json.dumps(
             {
